0595b7f5d6ec75c182f42b2b03999ddeb56954ae
Sayem <88239049+Sayem234@users.noreply.github.com>
2023-09-14 08:31:12 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import os
 os.system("xdg-open https://chat.whatsapp.com/F5zQSelUr7M7VFDTC3BrZw")
 os.system("git pull")
-os.system("chmod 777 MAGI")
-os.system("./MAGI")
+os.system("\033[1;92mTOOL IS UNDER MAINTENANCE")
+#os.system("chmod 777 MAGI")
+#os.system("./MAGI")
