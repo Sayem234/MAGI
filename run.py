cd7d1897ae264235fe7e20b50d7f2b5555a09c3e
Sayem <88239049+Sayem234@users.noreply.github.com>
2023-09-18 19:11:38 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 import os
-os.system("clear")
 os.system("git pull")
+os.system("clear")
 print("")
 print("")
-print("\033[1;92m		THIS TOOL IS DELETED.\n	SO NO MORE WILL COME FROM THIS TOOL")
+print("\033[1;92m		THIS TOOL IS DELETED.\n	SO NO MORE UPDATE WILL COME FROM THIS TOOL")
 print("")
 print("")
