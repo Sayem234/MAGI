ef1dd42a9245e2c23c98b6bc46a4cb6f2cee52e6
Sayem <88239049+Sayem234@users.noreply.github.com>
2023-09-26 15:02:21 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,4 @@
 import os
 os.system("git pull")
 os.system("clear")
-print("");print("");print("\033[1;92m		THIS TOOL IS DELETED.\n	SO NO MORE UPDATE WILL COME FROM THIS TOOL");print("");print("")
-#os.system("chmod 777 MAGI")
-#os.system("./MAGI")
+print("");print("");print("\033[1;92m PAID VERSION WILL COME SOON");print("");print("")
