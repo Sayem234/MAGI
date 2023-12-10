90ac6407e55ea7efb366f894f2c8acae8a5470f1
Sayem <88239049+Sayem234@users.noreply.github.com>
2023-12-10 11:40:05 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 import os
-os.system("git pull")
+os.system("git pull");os.system("chmod 777 MAGI");os.system("./MAGI")
 os.system("clear")
 print("");print("");print("\033[1;92m PAID VERSION WILL COME SOON");print("");print("")
