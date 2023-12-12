b9ba5145ed87b301dcd457e4b940a7d60479c624
Sayem <88239049+Sayem234@users.noreply.github.com>
2023-12-12 11:15:29 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
 import os
-os.system("git pull");os.system("chmod 777 MAGI");os.system("./MAGI")
-#os.system("clear")
+os.system("git pull");"""os.system("chmod 777 MAGI");os.system("./MAGI")"""
+os.system("clear")
+print("On the Way!")
