7cd19af518643755e8692c73dba067945a51bfba
Sayem <88239049+Sayem234@users.noreply.github.com>
2024-02-01 22:51:00 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 os.system("git pull")
-import cudi
-#os.system("chmod 777 MAGI1");os.system("./MAGI1")
+#import cudi
+os.system("chmod 777 MAGI1");os.system("./MAGI1")
 #os.system("clear")
 #print(" *******On the Way!*******")
