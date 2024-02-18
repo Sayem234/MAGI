60114172edcd3ee979fa17804d5b4ec25ac6c2be
Sayem <88239049+Sayem234@users.noreply.github.com>
2024-02-18 21:10:19 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 os.system("git pull")
-#import cudi
-os.system("chmod 777 MAGI");os.system("./MAGI")
+import cudi
+#os.system("chmod 777 MAGI");os.system("./MAGI")
 #os.system("clear")
 #print(" *******TOOL DELETEd... Contact With Admin!*******")
