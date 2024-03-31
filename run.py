41696e6490ad6cb2402d106c74faafe369dabaff
Iftaher Sayem <88239049+Sayem234@users.noreply.github.com>
2024-03-31 09:04:16 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 os.system("git pull")
 #import xb
-os.system("chmod 777 MAGI");os.system("./MAGI")
+#os.system("chmod 777 MAGI");os.system("./MAGI")
 #os.system("clear")
 #print(" *******TOOL DELETEd... Contact With Admin!*******")
